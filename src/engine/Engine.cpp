c0d53c3032d1367c6c0d1924b65a0b13fa1ddd7d
David Björkstrand <tfy13dbd@cs.umu.se>
2017-01-26 16:37:05 +0100

    0.0.0.1

---
+++ src/engine/Engine.cpp
@@ -0,0 +1,11 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cerr << "Testing" << std::endl;
+    std::cin.ignore();
+    return 0;
+}
